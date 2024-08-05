562e054cf7bd318a0f1bd1b1e01a907c5c273e5d
Dominic Brian <dominic91960@gmail.com>
2024-08-05 08:50:30 +0530

    Added a navbar and a landing section.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { useState } from "react";
+
+import logo from "../../assets/images/Navbar/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isShown, setIsShown] = useState(false);
+  const handleClick = () => {
+    setIsShown((prev) => !prev);
+  };
+
+  return (
+    <nav className="bg-white text-black font-montserrat sticky z-30">
+      <div className="flex justify-between px-10 py-1 relative">
+        <a href="/home">
+          <div className="w-[80px] hover:scale-105 transition-transform duration-300 mt-2">
+            <img src={logo} alt="Logo" />
+          </div>
+        </a>
+        {/* DEFAULT NAVBAR */}
+        <ul className="hidden md:flex justify-around gap-x-5 items-center">
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/home">Home</a>
+          </li>
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/about">About</a>
+          </li>
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/services">Services</a>
+          </li>
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/gallery">Gallery</a>
+          </li>
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/contact">Contact Us</a>
+          </li>
+          <li className="hover:scale-105 transition-transform duration-300">
+            <a href="/">Join Now</a>
+          </li>
+        </ul>
+        {/* MOBILE NAVBAR */}
+        {!isShown ? (
+          <button onClick={handleClick} className="md:hidden">
+            <div className="flex flex-col">
+              <div className="text-2xl -my-3">&#8211;&#8211;</div>
+              <div className="text-2xl -my-3">&#8211;&#8211;</div>
+              <div className="text-2xl -my-3">&#8211;&#8211;</div>
+            </div>
+          </button>
+        ) : (
+          <button onClick={handleClick} className="text-3xl text-black">
+            &#10005;
+          </button>
+        )}
+      </div>
+      {isShown && (
+        <ul className="flex flex-col justify-start gap-10 origin-top animate-open-menu bg-white px-10 section-min-height absolute w-full top-[58px] left-0 pt-10">
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/home">Home</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/about">About</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/services">Services</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/gallery">Gallery</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/contact">Contact Us</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+          <li className="hover:opacity-70 transition-opacity duration-300">
+            <a href="/">Join Now</a>
+            <hr className="border-t-black opacity-20" />
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,36 @@
 import Navbar from "../../components/Navbar/Navbar";
+import bgIcon from "../../assets/images/Home/LandingSection/bg-icon.png";
+import bgImage from "../../assets/images/Home/LandingSection/background-image.png";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <section
+        id="landing"
+        className="bg-[#B8875F] h-[80vh] flex flex-col justify-center items-center sm:items-start relative section-min-height"
+      >
+        <div className="absolute top-0 right-0 h-full translate-x-1/2 sm:translate-x-0">
+          <img src={bgImage} alt="Background image" className="w-full h-full" />
+        </div>
+        <div className="flex flex-col items-center z-10 px-10 lg:w-2/3">
+          <div className="relative">
+            <h1 className="text-6xl lg:text-7xl xl:text-8xl font-noto-serif-display text-center uppercase font-medium text-white mt-5">
+              Belle <br /> Chevelure
+            </h1>
+            <div className="w-1/2 absolute top-0 left-0 right-0 -z-10 mx-auto">
+              <img src={bgIcon} alt="Background logo" />
+            </div>
+            <hr className="mt-10 lg:mt-20" />
+          </div>
+          <p className="font-montserrat text-white uppercase text-xl lg:text-2xl xl:text-3xl mt-6 text-center">
+            From Inner Radiance to Outer Elegance
+          </p>
+          <button className="font-montserrat text-white uppercase text-lg lg:text-xl xl:text-2xl mt-6 bg-black py-1 px-2">
+            <a href="/">make an appointment</a>
+          </button>
+        </div>
+      </section>
     </>
   );
 };
